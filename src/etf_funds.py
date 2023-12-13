6517255fbe87177b2d438b0dac978216aa0d65cb
Jonny Costa Martinez <jcostamartin@factset.com>
2023-12-13 10:39:13 +0000

    using defined schema

--- src/etf_funds.py
+++ src/etf_funds.py
@@ -7,7 +7,20 @@ spark = SparkSession.builder \
 
 # load data from CSV file to DataFrame
 csv_file_path = "s3://fdss3-dbis-databricks-etl/jcostamartin/etf_funds.csv"
-df = spark.read.format("csv").option("inferSchema", "true").option("header", "true").option("delimiter", "|").load(csv_file_path)
+
+schema = StructType([
+    StructField("FACTSET_ENTITY_ID", StringType(), True),
+    StructField("FSYM_ID", StringType(), True),
+    StructField("REPORT_DATE", DateType(), True),
+    StructField("SHARES", DoubleType(), True),
+    StructField("PRICE", DoubleType(), True),
+    StructField("MARKET_VALUE", DoubleType(), True),
+    StructField("ISO_CURRENCY", StringType(), True),
+    StructField("MV_WEIGHT", DoubleType(), True)
+])
+
+#df = spark.read.format("csv").option("inferSchema", "true").option("header", "true").option("delimiter", "|").load(csv_file_path)
+df = spark.read.format("csv").option("mergeSchema", "false").schema(schema).option("header", "true").option("delimiter", "|").load(csv_file_path)
 
 # transform DataFrame as necessary 
 # since this is an example, I'm not performing any transformations
